568a22c566665fa81c38ed6577d74a5d5e0407e3
Tony Nguyen <tonynguyeenn@gmail.com>
2024-12-21 13:40:41 -0800

    Combined login screen and homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,29 @@
 
 import React, { useEffect, useState } from "react";
 import CardContainer from "@/components/cardContainer/cardContainer";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import Header from "@/components/header/header";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "./firebase";
 
 export default function Home() {
   const [user, setUser] = useState<User | null>(null); // Store the user state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("User logged in:", userCredential.user);
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -30,18 +47,47 @@ export default function Home() {
     );
   } else {
     return (
-      <div className="flex items-center justify-center gap-2">
-        <button className="">
-          <a
-            href="/login"
-            className="bg-[#E5D3FF] text-2xl px-4 py-1 border-[#CCA8FF] border-4 rounded-lg font-bold"
-          >
-            Login
-          </a>
-        </button>
-        <button className="border-4 border-[#CCA8FF] text-2xl px-4 py-1 rounded-lg font-bold">
-          <a href="/signUp">Sign Up</a>
-        </button>
+      <div className="flex items-center justify-center h-[calc(100vh-40px)]">
+        <div className="w-80 bg-[#E2E2E2] p-6 rounded-[20px] border-[#9E9E9E] border-4">
+          <h1 className="text-4xl font-bold text-center mb-[15px]">Login</h1>
+          <form onSubmit={handleLogin} className="flex flex-col gap-6">
+            <div>
+              <h2 className="text-2xl font-bold">Email</h2>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="border-b-[3px] border-b-black bg-transparent text-xl w-full"
+              />
+            </div>
+            <div>
+              <h2 className="text-2xl font-bold">Password</h2>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="border-b-[3px] border-b-black bg-transparent text-xl w-full"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="bg-[#E5D3FF] border-[#CCA8FF] border-[4px] rounded-lg py-1 font-bold text-xl"
+            >
+              Login
+            </button>
+            <h2 className="text-center">
+              Have no Account?
+              <span className="font-bold">
+                <a href="/signUp"> Sign up!</a>
+              </span>
+            </h2>
+          </form>
+        </div>
       </div>
     );
   }
